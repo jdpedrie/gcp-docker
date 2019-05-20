cc235195c5400ebd056b0c159b12457ebf64d2bf
John Pedrie <john@pedrie.com>
2019-05-20 12:14:26 -0400

    Add no-dev and ignore-platform-reqs

--- cli/src/Composer.php
+++ cli/src/Composer.php
@@ -17,19 +17,30 @@ class Composer extends Command
         $this->setName('composer')
             ->setDescription('Run Composer in Docker')
             ->addArgument('path', InputArgument::REQUIRED, 'The path of the project, relative to the workspace root.')
-            ->addArgument('cmd', InputArgument::REQUIRED, 'The composer argument');
+            ->addArgument('cmd', InputArgument::REQUIRED, 'The composer argument')
+            ->addOption('no-dev', null, InputOption::VALUE_NONE, 'If set, run install/update with --no-dev')
+            ->addOption('ignore-platform-reqs', null, InputOption::VALUE_NONE, 'If set, run with --ignore-platform-reqs');
 
         $this->addOptions();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $args = $input->getArgument('cmd');
+        if ($input->getOption('no-dev')) {
+            $args .= ' --no-dev';
+        }
+
+        if ($input->getOption('ignore-platform-reqs')) {
+            $args .= ' --ignore-platform-reqs';
+        }
+
         $cmd = sprintf(
             'run php %s',
             sprintf(
                 '/bin/bash -c "cd /gcp/%s; composer %s"',
                 $input->getArgument('path'),
-                $input->getArgument('cmd')
+                $args
             )
         );
 
